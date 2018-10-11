eef6630041e9553cca4bf782620218efd23ccee2
dxzhan <zhandx@163.com>
2018-10-11 10:16:45 +0800

    添加refreshdb模块，用于结构及测试数据的初始化 使用了antrun插件和dbunit

---
+++ refreshdb/db/sit/data_h2.sql
@@ -0,0 +1,1 @@
+insert  into `t_user`(`id`,`user_name`,`password`,`age`) values (1,'测试','123456',30);
---
+++ refreshdb/db/sit/schema_h2.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(40) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `age` int(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
