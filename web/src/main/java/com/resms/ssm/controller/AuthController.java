2e7303f7b4f0a772ca8a5290c88d072f3e7f9fa6
dxzhan <zhandx@163.com>
2018-10-21 19:25:43 +0800

    添加AuthController

---
+++ web/src/main/java/com/resms/ssm/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.resms.ssm.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.resms.ssm.auth.bean.User;
+import com.resms.ssm.auth.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping(value = "/rest/auth")
+public class AuthController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/user/{id}", method = { RequestMethod.GET })
+    public JsonNode json(@PathVariable String id) throws IOException {
+        logger.debug("============req id is " + id);
+
+        ObjectMapper mapper = new ObjectMapper();
+        User user = userService.getUserById(Long.parseLong(id));
+
+        String jsonText = mapper.writeValueAsString(user);
+
+        JsonNode node = mapper.readValue(jsonText,JsonNode.class);
+
+        return node;
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+}
