d9ceb0d6c83ce204292d1d4a3c371cc6ae7ed555
dxzhan <zhandx@163.com>
2019-02-10 08:33:15 +0800

    类型异常类

---
+++ web/src/main/java/com/resms/common/TypeException.java
@@ -0,0 +1,19 @@
+package com.resms.common;
+
+public class TypeException extends RuntimeException {
+    public TypeException() {
+        super();
+    }
+
+    public TypeException(String message) {
+        super(message);
+    }
+
+    public TypeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public TypeException(Throwable cause) {
+        super(cause);
+    }
+}
